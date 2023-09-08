f35a2a0cd2a94100ef50a73038090ec853e40502
Denis Emanov <denemanow@yandex.ru>
2023-09-08 12:00:19 +0300

    initial commit

---
+++ CMakeFiles/testfw_devboard.elf.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/silent/work/testfw_devboard/Core/Startup/startup_stm32f303vctx.s" "/home/silent/work/testfw_devboard/CMakeFiles/testfw_devboard.elf.dir/Core/Startup/startup_stm32f303vctx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F303xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F3xx_HAL_Driver/Inc"
+  "Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  "Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/silent/work/testfw_devboard/Core/Src/main.c" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/main.c.obj.d"
+  "/home/silent/work/testfw_devboard/Core/Src/stm32f3xx_hal_msp.c" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/stm32f3xx_hal_msp.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/stm32f3xx_hal_msp.c.obj.d"
+  "/home/silent/work/testfw_devboard/Core/Src/stm32f3xx_it.c" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/stm32f3xx_it.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/stm32f3xx_it.c.obj.d"
+  "/home/silent/work/testfw_devboard/Core/Src/syscalls.c" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/home/silent/work/testfw_devboard/Core/Src/sysmem.c" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/home/silent/work/testfw_devboard/Core/Src/system_stm32f3xx.c" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/system_stm32f3xx.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Core/Src/system_stm32f3xx.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj.d"
+  "/home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c.obj" "gcc" "CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/testfw_devboard.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj: \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ /home/silent/work/testfw_devboard/Core/Inc/stm32f3xx_hal_conf.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Include/core_cm4.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/silent/work/testfw_devboard/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_ll_usb.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h \
+ /home/silent/work/testfw_devboard/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h
